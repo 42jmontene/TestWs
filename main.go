e18f3c7113e6b50209f83883bc6d282d44fe4d38
root <root@remi-lab.42.fr>
2018-06-22 12:16:21 +0200

    MAJ

--- main.go
+++ main.go
@@ -9,20 +9,23 @@ import (
 )
 
 func main() {
-	fmt.Println("-----------------------------")
-	fmt.Println("LOGTIME", time.Now())
-	memory, err := memory.Get()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
-	}
-	fmt.Printf("\nmemory total : %d bytes\n\n", memory.Total)
-	fmt.Printf("memory used  : %d bytes      |      %d%%\n\n", memory.Used, memory.Used * 100 / memory.Total)
+	for {
+		fmt.Println("-----------------------------")
+		fmt.Println("LOGTIME", time.Now())
+		memory, err := memory.Get()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			return
+		}
+		fmt.Printf("\nmemory total : %d bytes\n\n", memory.Total)
+		fmt.Printf("memory used  : %d bytes      |      %d%%\n\n", memory.Used, memory.Used * 100 / memory.Total)
 
-	uptime, err := uptime.Get()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return
+		uptime, err := uptime.Get()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			return
+		}
+		fmt.Println("total uptime :", uptime, "\n")
+		time.Sleep(time.Minute)
 	}
-	fmt.Println("total uptime :", uptime, "\n")
 }
